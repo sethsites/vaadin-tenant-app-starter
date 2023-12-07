dfff089486dcd21ad55fb8af6d4b5a16ff9df0ef
start.vaadin.com <no-reply@vaadin.com>
2023-12-07 21:31:54 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/adminview/AdminViewView.java
@@ -0,0 +1,36 @@
+package com.example.application.views.adminview;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.TabSheet;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.RolesAllowed;
+
+@PageTitle("Admin View")
+@Route(value = "admin-view", layout = MainLayout.class)
+@RolesAllowed("ADMIN")
+@Uses(Icon.class)
+public class AdminViewView extends Composite<VerticalLayout> {
+
+    public AdminViewView() {
+        TabSheet tabSheet = new TabSheet();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        tabSheet.setWidth("100%");
+        tabSheet.getStyle().set("flex-grow", "1");
+        setTabSheetSampleData(tabSheet);
+        getContent().add(tabSheet);
+    }
+
+    private void setTabSheetSampleData(TabSheet tabSheet) {
+        tabSheet.add("Dashboard", new Div(new Text("This is the Dashboard tab content")));
+        tabSheet.add("Payment", new Div(new Text("This is the Payment tab content")));
+        tabSheet.add("Shipping", new Div(new Text("This is the Shipping tab content")));
+    }
+}
